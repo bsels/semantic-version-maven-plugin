386cb43cc8584fdc646960527ac51b6806e62124
Boris Sels <boris.sels@gmail.com>
2026-01-24 10:18:16 +0100

    Fix variable quoting in `update-readme.sh` for `GIT_STASH` check

--- update-readme.sh
+++ update-readme.sh
@@ -4,7 +4,7 @@ then
   sed "s/<version>[0-9]\+[.][0-9]\+[.][0-9]\+<\/version>/<version>$NEW_VERSION<\/version>/g" README.md
 else
   sed -i "s/<version>[0-9]\+[.][0-9]\+[.][0-9]\+<\/version>/<version>$NEW_VERSION<\/version>/g" README.md
-  if [ "GIT_STASH" = "true" ]
+  if [ "$GIT_STASH" = "true" ]
   then
     git add README.md
   fi
