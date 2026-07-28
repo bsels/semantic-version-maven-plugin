5b95928cee17c249dddcf280402bab31d88a1819
Depickere Sven <sven.depickere@hotmail.com>
2026-07-28 20:53:37 +0200

    Add package-info for `semantic.version.template` detailing changelog template support

---
+++ src/main/java/io/github/bsels/semantic/version/template/package-info.java
@@ -0,0 +1,4 @@
+/// Provides template-driven changelog entry support for the `create` goal:
+/// parsing template files with YAML front matter, prompting for variable values,
+/// rendering the template body, and resolving local or remote template locations.
+package io.github.bsels.semantic.version.template;
